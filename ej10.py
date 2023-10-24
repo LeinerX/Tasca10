35fa40db9a024c3df88e68b9758d84eb2564617a
LeinerX <lzapata20730@iesjoanramis.org>
2023-10-24 13:17:56 +0200

    Ej10

---
+++ ej10.py
@@ -0,0 +1,11 @@
+#Definición de la Función
+def mayor(a):
+    if x>18:
+        print("Es mayor de edad")
+    elif x<18:
+        print("No es mayor de edad")
+    else:
+        print("Tiene 18 anos")
+#Programa principal
+x = int(input("Introduzca su edad: "))
+mayor(x)
